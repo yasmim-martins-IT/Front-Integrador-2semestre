3e4f7b65ad3d21debd2fb67d6063fda03bce3156
Yasmim Martins <yasmim.martins.profissional@gmail.com>
2025-05-27 19:57:15 -0300

    3.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Routas } from './routes/Routes'
+import './App.css'
 
 export function App() {
   return (
--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,11 +1,12 @@
-import styles from './Aside.module.css'
-import logo from "../assets/Logo.jpg"
+import styles from './Aside.module.css';
+import logo from '../assets/Logo.png'
 import { FaChalkboardTeacher } from "react-icons/fa";
-import { GoBook } from "react-icons/go";
-import { HiAcademicCap } from "react-icons/hi2";
-import { FaTasks } from "react-icons/fa";
+import { FaTemperatureHalf } from "react-icons/fa6";
+import { RiHomeLine } from "react-icons/ri";
+import { ImDroplet } from "react-icons/im";
 import {Link} from 'react-router' ; 
-
+import { BsLightbulb } from "react-icons/bs";
+import { CgNotes } from "react-icons/cg";
 export function Aside(){
     return(
         <aside className={styles.container}>
@@ -17,12 +18,28 @@ export function Aside(){
 
            </header>
            <section className={styles.containerBotoes}>
-                <Link className={styles.botao} to = {'professor'}>
-                    <FaChalkboardTeacher className={styles.icone} size={30} />
-                    Professor</Link>
-                <Link className={styles.botao} to = {'disciplinas'}><GoBook className={styles.icone} size={25}  />Disciplinas</Link>
-                <Link className={styles.botao} to = {'sala'}><HiAcademicCap className={styles.icone} size={30} />Ambientes</Link>
-                <Link className={styles.botao} to = {'/initial'}><FaTasks className={styles.icone} size={25} /> Reservar</Link>
+             <div className={styles.links_container}>
+
+                    <div className={styles.container_icons}>
+                        <ImDroplet className={styles.icone} size={30} />
+                        <FaTemperatureHalf className={styles.icone} size={30} />
+                        <RiHomeLine className={styles.icone} size={30} />
+                        <BsLightbulb className={styles.icone} size={30} />
+                        <CgNotes className={styles.icone} size={30} />
+                    </div>
+
+                    <div className={styles.container_links}>
+                        <Link className={styles.botao} to={'/Umidade'}>
+                            Umidade
+                        </Link>
+                        <Link className={styles.botao} to={'/Iluminacao'}>Iluminação</Link>
+                        <Link className={styles.botao} to={'/Temperatura'}>Temperatura</Link>
+                        <Link className={styles.botao} to={'/Ambientes'}>Ambientes</Link>
+                        <Link className={styles.botao} to={'/Historico'}>Histórico</Link>
+                    </div>
+
+                </div>
+                
            </section>
         </aside>
     )

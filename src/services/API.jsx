25b5b63e313a14078898b1eeaa8a1f79fb720ee5
Yasmim Sampaio Martins <53661815857@>
2025-06-02 14:51:36 -0300

    version 9.0

--- src/services/API.jsx
+++ src/services/API.jsx
@@ -2,9 +2,9 @@ import {useState , useEffect} from "react";
 import axios from "axios" 
 const API_URL = process.env.REACT_APP_API_URL
 
-export async function  getFunction() {
+export async function  getFunctionHistorico() {
 try{
-    const response = axios.get(`${API_URL}/`);
+    const response = axios.get(`${API_URL}/visualizarHistorico/`);
     
     return response.data ; 
 
